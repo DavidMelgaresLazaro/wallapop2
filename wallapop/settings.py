20c7700b7ce573e7a16384ae403809e163798839
joanjaume <fotosjoanj@gmail.com>
2023-02-27 19:55:51 +0100

    me

--- wallapop/settings.py
+++ wallapop/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = '../images'
+MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
