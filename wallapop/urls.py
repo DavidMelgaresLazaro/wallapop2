08d137a206b2a06c7091b3f9c1970d1baf221a71
emuajj <80452243+emuajj@users.noreply.github.com>
2023-04-25 18:02:49 +0200

    primer

--- wallapop/urls.py
+++ wallapop/urls.py
@@ -17,12 +17,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import User
 from wallapop_app.views import anunci_view,SignUpView,edit_profile,get_anunci,ChangePasswordView,afegiranunci,veureperfil,afegiranunci
 
+from rest_framework import routers
+from poster import views
 
 from django.urls import path
 
 
+
+router = routers.DefaultRouter()
+router.register(r'anuncis', views.PostViewSet)
 
 
 urlpatterns = [
@@ -35,6 +41,8 @@ urlpatterns = [
     path('users/<str:name>/', veureperfil, name='users'),
     path('password-change/', ChangePasswordView.as_view(), name='password-change'),
     path('add_anunci/', afegiranunci ,name='add_anunci'),
+
+    path('api-auth/', include('rest_framework.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
