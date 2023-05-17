614cd50b109b15825fc9c4850e22ff437647aeb8
emuajj <80452243+emuajj@users.noreply.github.com>
2023-05-17 17:57:08 +0200

    commit

--- static/javascript/anunciv.js
+++ static/javascript/anunciv.js
@@ -1,15 +1,37 @@
 const objectList = document.getElementById("anuncis-list");
+const dataContainer = document.getElementById('anuncis-list');
 
-fetch("/api/anuncis")
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      const object = data[i];
-      const nameItem = document.createElement("li");
-      const dataItem = document.createElement("li");
-      objectItem.innerText = object.name;
-      objectItem.innerText = object.d
-      objectList.appendChild(objectItem);
-    }
-  })
-  .catch(error => console.error(error));
+// fetch("/api/anuncis")
+//   .then(response => response.json())
+//   .then(data => {
+//     for (let i = 0; i < data.length; i++) {
+//       const object = data[i];
+//       const nameItem = document.createElement("li");
+//       const dataItem = document.createElement("li");
+//       objectItem.innerText = object.name;
+//       objectItem.innerText = object.d
+//       objectList.appendChild(objectItem);
+//     }
+//   })
+//   .catch(error => console.error(error));
+
+  fetch('/api/anuncis/')
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(anunci => {
+                    const item = document.createElement('div');
+                    item.innerHTML = `
+                        <img src="${anunci.foto}" width="500" height="300"></a>
+                        <h2>${anunci.titol}</h2>
+                        <p>Posted by: ${anunci.name.username}</p>
+                        <p>Date: ${anunci.data}</p>
+                        <p>Description: ${anunci.description}</p>
+                        <p>Price: $${anunci.preu}</p>
+                        <hr>
+                    `;
+                    dataContainer.appendChild(item);
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
